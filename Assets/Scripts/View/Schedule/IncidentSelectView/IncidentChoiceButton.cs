19ab25b9ecd8504093ba2f60a67028a081dc0ab5
abyss_tutor <yejun6210@gmail.com>
2026-03-07 02:07:10 +0900

    [Feat] 사건 노드 UX 제작 완료

--- Assets/Scripts/View/Schedule/IncidentSelectView/IncidentChoiceButton.cs
+++ Assets/Scripts/View/Schedule/IncidentSelectView/IncidentChoiceButton.cs
@@ -1,9 +1,38 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 namespace View.ScheduleView.IncidentSelectView
 {
-    public class IncidentChoiceButton : MonoBehaviour {
+    public class IncidentChoiceButton : MonoBehaviour 
+    {
         [SerializeField] private TextMeshProUGUI mainText;
+        private Action onPressed;
+
+        public void Initiate(DeterminedIncidentChoice choice, Action onPressed)
+        {
+            Unactive();
+
+            mainText.text = choice.Description;
+            this.onPressed = onPressed;
+            
+            gameObject.SetActive(true);
+        }
+
+        public void Unactive()
+        {
+            onPressed = null;
+            gameObject.SetActive(false);
+        }
+
+        public void OnPressed()
+        {
+            if (onPressed == null) return;
+
+            var actionToInvoke = onPressed;
+            Unactive();
+
+            actionToInvoke.Invoke();
+        }
     }
 }
--- Assets/Scripts/View/Schedule/IncidentSelectView/IncidentSelectView.cs
+++ Assets/Scripts/View/Schedule/IncidentSelectView/IncidentSelectView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 using View.Core;
 using ViewEvent.Core;
 using ViewEvent.ScheduleView;
@@ -9,11 +11,31 @@ namespace View.ScheduleView.IncidentSelectView
     public class IncidentSelectView : InteractableViewBehaviour<IScheduleViewEvent, IScheduleViewCommander>
     {
         [SerializeField] private GameObject uiRoot;
+        [SerializeField] private Transform buttonsContainer; 
+        [SerializeField] private GameObject incidentButtonPrefab; 
+
+        private IObjectPool<IncidentChoiceButton> pool;
+        private List<IncidentChoiceButton> activeButtons = new List<IncidentChoiceButton>();
 
         public override void OnInitialized()
         {
             uiRoot.SetActive(false);
 
+            pool = new ObjectPool<IncidentChoiceButton>(
+                createFunc: () =>
+                {
+                    var buttonObj = Instantiate(incidentButtonPrefab, buttonsContainer);
+                    buttonObj.SetActive(false);
+                    return buttonObj.GetComponent<IncidentChoiceButton>();
+                },
+                actionOnGet: (button) => { button.gameObject.SetActive(true); },
+                actionOnRelease: (button) => { button.Unactive(); },
+                actionOnDestroy: (button) => { Destroy(button.gameObject); },
+                defaultCapacity: 4,
+                maxSize: 10
+            );
+
+            activeButtons.Clear();
             eventBus.Subscribe<IncidentSelectRequested>(OnIncidentSelectRequested);
         }
         public override void OnDestroy()
@@ -23,10 +45,29 @@ namespace View.ScheduleView.IncidentSelectView
 
         public void OnIncidentSelectRequested(IncidentSelectRequested payload)
         {
+            uiRoot.SetActive(true);
+
             foreach (var choice in payload.Choices)
             {
-                Debug.Log($"{choice.Description}");
+                var button = pool.Get();
+                
+                button.Initiate(choice, () => OnChoiceSelected(choice));
+                
+                activeButtons.Add(button);
             }
+        }
+
+        private void OnChoiceSelected(DeterminedIncidentChoice selectedChoice)
+        {
+            commander.SettleIncidentChoice(selectedChoice);
+
+            foreach (var button in activeButtons)
+            {
+                pool.Release(button);
+            }
+            activeButtons.Clear();
+
+            uiRoot.SetActive(false);
         }
     }
 }
