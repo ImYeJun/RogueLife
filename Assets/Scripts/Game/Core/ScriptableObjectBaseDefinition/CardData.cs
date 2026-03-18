08668838b4dd9da8e690e746f494fd277dedb4eb
abyss_tutor <yejun6210@gmail.com>
2026-03-19 02:04:05 +0900

    [Feat] CardData에 관련된 BattleStatusEffect Id 기재함

--- Assets/Scripts/Game/Core/ScriptableObjectBaseDefinition/CardData.cs
+++ Assets/Scripts/Game/Core/ScriptableObjectBaseDefinition/CardData.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CardData", menuName = "Scriptable Objects/CardData")]
@@ -7,7 +8,9 @@ public class CardData : ScriptableObject
     [SerializeField] private string cardName;
     [SerializeField] private Sprite background;
     [SerializeField, TextArea] private string description;
+    [SerializeField] private List<string> relatedStatusEffectIds;
     [SerializeField, TextArea] private string relfectionAppliedDescription;
+    [SerializeField] private List<string> reflectionRelatedStatusEffectIds;
     [SerializeField] private CardType type;
     [SerializeField] private CardAttribute attribute;
     [SerializeField] private CardRarity rarity;
@@ -22,4 +25,6 @@ public class CardData : ScriptableObject
     public CardRarity Rarity { get => rarity; }
     public int ActionCost { get => actionCost; }
     public Sprite Background { get => background; }
+    public List<string> RelatedStatusEffectIds { get => relatedStatusEffectIds; }
+    public List<string> ReflectionRelatedStatusEffectIds { get => reflectionRelatedStatusEffectIds; }
 }
